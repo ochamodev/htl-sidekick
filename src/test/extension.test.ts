75dab16d0ed3a49003ca9859e542b264c5ddc0b9
Otto Chamo <cheleyotto98@gmail.com>
2025-09-17 18:12:09 -0600

    feat: setup tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,41 @@
-import * as assert from 'assert';
+import * as vscode from "vscode";
+import { expect } from "chai";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite("HTL Helper – Hover & Completion", () => {
+  test("Hover over data-sly-list and display explanation", async () => {
+    const doc = await vscode.workspace.openTextDocument({
+      language: "html",
+      content: `<ul data-sly-list="\${items}"><li>\${item}</li></ul>`
+    });
+    const editor = await vscode.window.showTextDocument(doc);
+    const idx = doc.getText().indexOf("data-sly-list");
+    const pos = doc.positionAt(idx + 5);
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+    const hovers = await vscode.commands.executeCommand<vscode.Hover[]>(
+      "vscode.executeHoverProvider",
+      doc.uri,
+      pos
+    );
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    expect(hovers && hovers.length).to.be.greaterThan(0);
+    const md = hovers![0].contents[0] as vscode.MarkdownString;
+    expect(md.value).to.contain("Itera");
+    await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+  });
+
+  test("Autocompletado sugiere data-sly-test dentro de etiqueta", async () => {
+    const doc = await vscode.workspace.openTextDocument({
+      language: "html",
+      content: `<div da`
+    });
+    await vscode.window.showTextDocument(doc);
+    const pos = new vscode.Position(0, 7); // after 'da'
+    const list = await vscode.commands.executeCommand<vscode.CompletionList>(
+      "vscode.executeCompletionItemProvider",
+      doc.uri,
+      pos
+    );
+    const labels = (list?.items || []).map((i) => i.label);
+    expect(labels).to.include("data-sly-test");
+  });
 });
